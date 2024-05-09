4fd830a876bf1d51ce61858e226918898d8f4803
Justin Nipper <justin.nipper@me.com>
2024-05-09 12:26:49 -0500

    Initial Commit

---
+++ BugsnagUploader/BugsnagUploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugsnagUploaderApp.swift
+//  BugsnagUploader
+//
+//  Created by Justin Nipper on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BugsnagUploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BugsnagUploader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BugsnagUploader
+//
+//  Created by Justin Nipper on 5/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
